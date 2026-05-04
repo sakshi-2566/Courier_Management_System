16fb776cbb142adca6b4acc25cc529b5df798e75
Sakshi H <sakshibh52@gmail.com>
2026-05-04 11:46:17 +0530

    Add files via upload

---
+++ courier-management-system-main/home/footer.php
@@ -0,0 +1,27 @@
+<!-- copyright and contactUs fixed footer -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.footer {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    
+    bottom: 0;
+    
+    color: white;
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+
+  <p style="margin-top: 4px;margin-bottom:-10px"></p><br>
+  <a href="mailto:premkumar1215225@gmail.com" style="color:brown"></a>
+
+</footer>
+
+</body>
+</html>
